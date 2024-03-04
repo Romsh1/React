42f947cf5e6e78cc601081f10bdca9e157c1ba31
Romika <romika2002chau.21@gmail.com>
2024-03-04 00:02:14 -0500

    Learned about creating react component - Day 29 of learning JS and React

---
+++ react-by-mosh/src/Message.tsx
@@ -0,0 +1,12 @@
+// Romika Chaudhary
+// Day 29
+
+function Message() {
+    const name = 'Romsh';
+    //JSX
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
+}
+
+export default Message;
