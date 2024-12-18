61523eb1e65c8f3e01ff708c6fa926b21620213a
Romika <romika2002chau.21@gmail.com>
2024-12-17 19:12:12 -0500

    Modified

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,28 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Greet from './components/Greet.js';
-import Welcome from './components/Welcome.js';
-// import Welcome from './components/Welcome.js';
-// import Hello from './components/Hello.js';
+import Counter from './components/Counter.js';
 
 class App extends Component () {
   render() {
     return (
-      <div className="App">
-        <Greet name = "Baby" heroName = "Ranger">
-          <p>This is a children props</p></Greet>
-        <Greet name= "Bae" heroName = "Batman">
-          <button>Click Me</button></Greet>
-        <Greet name = "Boo" heroName = "Perman" />
-        <Welcome name = "Baby" heroName = "Ranger">
-          <p>This is a children props</p></Welcome>
-        <Welcome name= "Bae" heroName = "Batman">
-          <button>Click Me</button></Welcome>
-        <Welcome name = "Boo" heroName = "Perman" />
-
-        {/* <Greet />
-        <Welcome /> */}
-        {/* <Hello /> */}
+      <div>
+        <Counter />
       </div>
     );  
   }
