360e02d357d8b4c04031ac364df1c5ad0e68b3d5
Romika <romika2002chau.21@gmail.com>
2024-12-26 17:57:10 -0500

    Done with food recipe application

--- foodapp/foodrecipe/src/components/ItemList.jsx
+++ foodapp/foodrecipe/src/components/ItemList.jsx
@@ -1,12 +1,10 @@
+import Item from "./Item.jsx";
+
 export default function ItemList({ food, isLoading }) {
     return (
         <div>
             {isLoading ? <p>Loading...</p> : food.extendedIngredients.map((item) => (
-                <div>
-                    <img src={`https://spoonacular.com/cdn/ingredients_100x100/`+ item.image} alt="" />
-                    <h3>{item.name}</h3>
-                    <h3>{item.amount}{item.unit}</h3>
-                </div>
+                <Item item={item} />
             ))}
             {}
         </div>
